1a04f58a0b7183be6a3ece08e19f8fd6cfad65db
李赫 <lihe@lihedeMacBook-Pro.local>
2022-03-19 21:31:49 +0800

    chapter 13: attributes

--- src/main.rs
+++ src/main.rs
@@ -572,7 +572,7 @@ fn main3_2() {
     println!("{}", x.run(1, 5));
 
 }
-
+/*
 fn main3_2_1() {
     enum Status {
         Rich,
@@ -584,9 +584,9 @@ fn main3_2_1() {
         Soldier,
     }
     // 显式地 `use` 各个名称使他们直接可用，而不需要指定它们来自 `Status`。
-    use Status::{Poor, Rich};
+    // use Status::{Poor, Rich};
     // 自动地 `use` `Work` 内部的各个名称。
-    use Work::*;
+    // use Work::*;
 
     // `Poor` 等价于 `Status::Poor`。
     let status = Poor;
@@ -605,6 +605,7 @@ fn main3_2_1() {
         Soldier  => println!("Soldiers fight!"),
     }
 }
+ */
 use List::*;
 fn main3_2_2() {
     // 拥有隐式辨别值（implicit discriminator，从 0 开始）的 enum
@@ -1839,7 +1840,7 @@ fn main9_4() {
     let a: () = some_fn();
     println!("This function returns and you can see this line.");
 
-    let x: ! = panic!("This call never returns.");
+    // let x: ! = panic!("This call never returns.");
     println!("You will never see this line!");
     fn sum_odd_numbers(up_to: u32) -> u32 {
         let mut acc = 0;
@@ -1859,6 +1860,285 @@ fn main9_4() {
     }
     println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
 }
+// 一个名为 `my_mod` 的模块
+mod my_mod {
+    use std::{print, println};
+
+    // 模块中的项默认具有私有的可见性
+    fn private_function() {
+        println!("called `my_mod::private_function()`");
+    }
+
+    // 使用 `pub` 修饰语来改变默认可见性。
+    pub fn function() {
+        println!("called `my_mod::function()`");
+    }
+
+    // 在同一模块中，项可以访问其它项，即使它是私有的。
+    pub fn indirect_access() {
+        print!("called `my_mod::indirect_access()`, that\n> ");
+        private_function();
+    }
+
+    // 模块也可以嵌套
+    pub mod nested {
+        pub fn function() {
+            println!("called `my_mod::nested::function()`");
+        }
+
+        #[allow(dead_code)]
+        fn private_function() {
+            println!("called `my_mod::nested::private_function()`");
+        }
+
+        // 使用 `pub(in path)` 语法定义的函数只在给定的路径中可见。
+        // `path` 必须是父模块（parent module）或祖先模块（ancestor module）
+        pub(in crate::my_mod) fn public_function_in_my_mod() {
+            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n > ");
+            public_function_in_nested()
+        }
+
+        // 使用 `pub(self)` 语法定义的函数则只在当前模块中可见。
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nested::public_function_in_nested");
+        }
+
+        // 使用 `pub(super)` 语法定义的函数只在父模块中可见。
+        pub(super) fn public_function_in_super_mod() {
+            println!("called my_mod::nested::public_function_in_super_mod");
+        }
+    }
+
+    pub fn call_public_function_in_my_mod() {
+        print!("called `my_mod::call_public_funcion_in_my_mod()`, that\n> ");
+        nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
+    }
+
+    // `pub(crate)` 使得函数只在当前 crate 中可见
+    pub(crate) fn public_function_in_crate() {
+        println!("called `my_mod::public_function_in_crate()");
+    }
+
+    // 嵌套模块的可见性遵循相同的规则
+    mod private_nested {
+        #[allow(dead_code)]
+        pub fn function() {
+            println!("called `my_mod::private_nested::function()`");
+        }
+    }
+}
+
+fn function() {
+    println!("called `function()`");
+}
+fn main10() {
+    // 模块机制消除了相同名字的项之间的歧义。
+    function();
+    my_mod::function();
+
+    // 公有项，包括嵌套模块内的，都可以在父模块外部访问。
+    my_mod::indirect_access();
+    my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
+
+    // pub(crate) 项可以在同一个 crate 中的任何地方访问
+    my_mod::public_function_in_crate();
+
+    // pub(in path) 项只能在指定的模块中访问
+    // 报错！函数 `public_function_in_my_mod` 是私有的
+    // my_mod::nested::public_function_in_my_mod();
+    // 试一试 ^ 取消该行的注释
+
+    // 模块的私有项不能直接访问，即便它是嵌套在公有模块内部的
+
+    // 报错！`private_function` 是私有的
+    //my_mod::private_function();
+    // 试一试 ^ 取消此行注释
+
+    // 报错！`private_function` 是私有的
+    //my_mod::nested::private_function();
+    // 试一试 ^ 取消此行的注释
+
+    // Error! `private_nested` is a private module
+    //my_mod::private_nested::function();
+    // 试一试 ^ 取消此行的注释
+}
+
+mod my10_2 {
+    // 一个公有的结构体，带有一个公有的字段（类型为泛型 `T`）
+    pub struct OpenBox<T> {
+        pub contents: T,
+    }
+
+    // 一个公有的结构体，带有一个私有的字段（类型为泛型 `T`）
+    #[allow(dead_code)]
+    pub struct ClosedBox<T> {
+        contents: T,
+    }
+
+    impl<T> ClosedBox<T> {
+        // 一个公有的构造器方法
+        pub fn new(contents: T) -> ClosedBox<T> {
+            ClosedBox {
+                contents: contents,
+            }
+        }
+    }
+}
+
+fn main10_2() {
+    // 带有公有字段的公有结构体，可以像平常一样构造
+    let open_box = my10_2::OpenBox { contents: "public information" };
+
+    // 并且它们的字段可以正常访问到。
+    println!("The open box contains: {}", open_box.contents);
+
+    // 带有私有字段的公有结构体不能使用字段名来构造。
+    // 报错！`ClosedBox` 含有私有字段。
+    //let closed_box = my::ClosedBox { contents: "classified information" };
+    // 试一试 ^ 取消此行注释
+
+
+    // 不过带有私有字段的结构体可以使用公有的构造器来创建。
+    let _closed_box = my10_2::ClosedBox::new("classified information");
+
+    // 并且一个结构体中的私有字段不能访问到。
+    // 报错！`content` 字段是私有的。
+    // println!("The closed box contains: {}", _closed_box.contents);
+    // 试一试 ^ 取消此行注释
+
+}
+// 将 `deeply::nested::function` 路径绑定到 `other_function`。
+use deeply::nested::function as other_function;
+
+mod deeply {
+    pub mod nested {
+        pub fn function() {
+            println!("called `deeply::nested::function()`")
+        }
+    }
+}
+
+fn main10_3() {
+    // 更容易访问 `deeply::nested::funcion`
+    other_function();
+
+    println!("Entering block");
+    {
+        // 这和 `use deeply::nested::function as function` 等价。
+        // 此 `function()` 将遮蔽外部的同名函数。
+        use deeply::nested::function;
+        function();
+
+        // `use` 绑定拥有局部作用域。在这个例子中，`function()`
+        // 的遮蔽只存在在这个代码块中。
+        println!("Leaving block");
+    }
+
+    function();
+}
+
+mod cool {
+    pub fn function() {
+        println!("called `cool::function()`");
+    }
+}
+
+mod my {
+    fn function() {
+        println!("called `my::function()`");
+    }
+
+    mod cool {
+        pub fn function() {
+            println!("called `my::cool::function()`");
+        }
+    }
+
+    pub fn indirect_call() {
+        // 让我们从这个作用域中访问所有名为 `function` 的函数！
+        print!("called `my::indirect_call()`, that\n> ");
+
+        // `self` 关键字表示当前的模块作用域——在这个例子是 `my`。
+        // 调用 `self::function()` 和直接调用 `function()` 都得到相同的结果，
+        // 因为他们表示相同的函数。
+        self::function();
+        function();
+
+        // 我们也可以使用 `self` 来访问 `my` 内部的另一个模块：
+        self::cool::function();
+
+        // `super` 关键字表示父作用域（在 `my` 模块外面）。
+        super::function();
+
+        // 这将在 *crate* 作用域内绑定 `cool::function` 。
+        // 在这个例子中，crate 作用域是最外面的作用域。
+        {
+            use crate::cool::function as root_function;
+            root_function();
+        }
+    }
+}
+
+fn main10_4() {
+    my::indirect_call();
+}
+
+mod my10_5;
+
+fn main10_5() {
+    my10_5::function();
+}
+
+fn main11_2() {
+    // rary::public_function();
+
+    // 报错！ `private_function` 是私有的
+    //rary::private_function();
+
+    // rary::indirect_access();
+}
+
+pub mod chapter13 {
+    // 这个函数仅当目标系统是 Linux 的时候才会编译
+    #[cfg(target_os = "linux")]
+    fn are_you_on_linux() {
+        println!("You are running linux!")
+    }
+
+    // 而这个函数仅当目标系统 **不是** Linux 时才会编译
+    #[cfg(not(target_os = "linux"))]
+    fn are_you_on_linux() {
+        println!("You are *not* running linux!")
+    }
+
+    pub fn main13_3() {
+        are_you_on_linux();
+
+        println!("Are you sure?");
+        if cfg!(target_os = "linux") {
+            println!("Yes. It's definitely linux!");
+        } else {
+            println!("Yes. It's definitely *not* linux!");
+        }
+    }
+}
+
+fn main13_1() {
+    fn used_function() {}
+
+    // `#[allow(dead_code)]` 属性可以禁用 `dead_code` lint
+    #[allow(dead_code)]
+    fn unused_function() {}
+
+    #[allow(dead_code)]
+    fn noisy_unused_function() {}
+    // 改正 ^ 增加一个属性来消除警告
+
+    used_function();
+}
 fn main() {
-    main9_4();
+    chapter13::main13_3();
 }
---
+++ test/my_test.rs
@@ -0,0 +1,6 @@
+
+// todo "Rust Programing Language" Chapter 11
+#[test]
+fn test01() {
+    println!("hello test");
+}
