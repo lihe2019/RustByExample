159908f8cf64b8535bf08c3f68b0dc5235941844
李赫 <lihe@lihedeMacBook-Pro.local>
2022-03-19 16:47:20 +0800

    champter6

--- src/main.rs
+++ src/main.rs
@@ -923,6 +923,85 @@ fn main5_4() {
              nanoseconds + inches);
 }
 
+fn main6_1() {
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+    use std::convert::From;
+
+    #[derive(Debug)]
+    struct Number {
+        value: i32,
+    }
+
+    impl From<i32> for Number {
+        fn from(item: i32) -> Self {
+            Number { value: item }
+        }
+    }
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+
+
+    let int = 5;
+    // 试试删除类型说明
+    let num: Number = int.into();
+    // let num = int.into();
+    println!("My number is {:?}", num);
+}
+
+fn main6_2() {
+    use std::convert::TryFrom;
+    use std::convert::TryInto;
+
+    #[derive(Debug, PartialEq)]
+    struct EvenNumber(i32);
+
+    impl TryFrom<i32> for EvenNumber {
+        type Error = ();
+
+        fn try_from(value: i32) -> Result<Self, Self::Error> {
+            if value % 2 == 0 {
+                Ok(EvenNumber(value))
+            } else {
+                Err(())
+            }
+        }
+    }
+
+    // TryFrom
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+}
+fn main6_3() {
+    struct Circle {
+        radius: i32
+    }
+
+    impl ToString for Circle {
+        fn to_string(&self) -> String {
+            format!("Circle of radius {:?}", self.radius)
+        }
+    }
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    // let parsed1 = "22".parse();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!{"Sum: {:?}", sum};
+}
+
 fn main() {
-    main5_4();
+    main6_3();
 }
