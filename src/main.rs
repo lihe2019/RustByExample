519f221875701e71d99c6f1927f2f6fb17c8095f
李赫 <lihe@lihedeMacBook-Pro.local>
2022-03-19 20:08:43 +0800

    champter 9, there are some points to review

--- src/main.rs
+++ src/main.rs
@@ -1486,6 +1486,379 @@ fn main8_7() {
     // ^ `if let` 有可选的 `else`/`else if` 分句，
     // 而 `while let` 没有。
 }
+
+fn main9() {
+    // 一个返回布尔值的函数
+    fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+        // 边界情况，提前返回
+        if rhs == 0 {
+            return false;
+        }
+
+        // 这是一个表达式，可以不用 `return` 关键字
+        lhs % rhs == 0
+    }
+
+    // 一个 “不” 返回值的函数。实际上会返回一个单元类型 `()`。
+    fn fizzbuzz(n: u32) -> () {
+        if is_divisible_by(n, 15) {
+            println!("fizzbuzz");
+        } else if is_divisible_by(n, 3) {
+            println!("fizz");
+        } else if is_divisible_by(n, 5) {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // 当函数返回 `()` 时，函数签名可以省略返回类型
+    fn fizzbuzz_to(n: u32) {
+        for n in 1..=n {
+            fizzbuzz(n);
+        }
+    }
+    fizzbuzz_to(100);
+}
+
+fn main9_1() {
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    // 实现的代码块，`Point` 的所有方法都在这里给出
+    impl Point {
+        // 这是一个静态方法（static method）
+        // 静态方法不需要被实例调用
+        // 这类方法一般用作构造器（constructor）
+        fn origin() -> Point {
+            Point { x: 0.0, y: 0.0 }
+        }
+
+        // 另外一个静态方法，需要两个参数：
+        fn new(x: f64, y: f64) -> Point {
+            Point { x: x, y: y }
+        }
+    }
+
+    struct Rectangle {
+        p1: Point,
+        p2: Point,
+    }
+
+    impl Rectangle {
+        // 这是一个实例方法（instance method）
+        // `&self` 是 `self: &Self` 的语法糖（sugar），其中 `Self` 是方法调用者的
+        // 类型。在这个例子中 `Self` = `Rectangle`
+        fn area(&self) -> f64 {
+            // `self` 通过点运算符来访问结构体字段
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+
+            // `abs` 是一个 `f64` 类型的方法，返回调用者的绝对值
+            ((x1 - x2) * (y1 - y2)).abs()
+
+            // ((self.p1.x - self.p2.x)*(self.p1.y - self.p2.y)).abs()
+        }
+
+        fn perimeter(&self) -> f64 {
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+
+            2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+        }
+
+        // 这个方法要求调用者是可变的
+        // `&mut self` 为 `self: &mut Self` 的语法糖
+        fn translate(&mut self, x: f64, y: f64) {
+            self.p1.x += x;
+            self.p2.x += x;
+
+            self.p1.y += y;
+            self.p2.y += y;
+        }
+    }
+
+    // `Pair` 拥有资源：两个堆分配的整型
+    struct Pair(Box<i32>, Box<i32>);
+
+    impl Pair {
+        // 这个方法会 “消耗” 调用者的资源
+        // `self` 为 `self: Self` 的语法糖
+        fn destroy(self) {
+            // 解构 `self`
+            let Pair(first, second) = self;
+
+            println!("Destroying Pair({}, {})", first, second);
+
+            // `first` 和 `second` 离开作用域后释放
+        }
+    }
+
+    let rectangle = Rectangle {
+        // 静态方法使用双冒号调用
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    // 实例方法通过点运算符来调用
+    // 注意第一个参数 `&self` 是隐式传递的，亦即：
+    // `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    // 报错！ `rectangle` 是不可变的，但这方法需要一个可变对象
+    //rectangle.translate(1.0, 0.0);
+    // 试一试 ^ 去掉此行的注释
+
+    // 正常运行！可变对象可以调用可变方法
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+
+    // 报错！前面的 `destroy` 调用 “消耗了” `pair`
+    // pair.destroy();
+    // 试一试 ^ 将此行注释去掉
+}
+
+fn main9_2() {
+    // 通过闭包和函数分别实现自增。
+    // 译注：下面这行是使用函数的实现
+    fn  function            (i: i32) -> i32 { i + 1 }
+
+    // 闭包是匿名的，这里我们将它们绑定到引用。
+    // 类型标注和函数的一样，不过类型标注和使用 `{}` 来围住函数体都是可选的。
+    // 这些匿名函数（nameless function）被赋值给合适地命名的变量。
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred  = |i     |          i + 1  ;
+
+    // 译注：将闭包绑定到引用的说法可能不准。
+    // 据[语言参考](https://doc.rust-lang.org/beta/reference/types.html#closure-types)
+    // 闭包表达式产生的类型就是 “闭包类型”，不属于引用类型，而且确实无法对上面两个
+    // `closure_xxx` 变量解引用。
+
+    let i = 1;
+    // 调用函数和闭包。
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    // 没有参数的闭包，返回一个 `i32` 类型。
+    // 返回类型是自动推导的。
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+    let my = || {println!("hello"); 1};
+
+    println!("{}", my());
+}
+
+fn main9_2_1() {
+    use std::mem;
+
+    let color = String::from("green");
+
+    // 这个闭包打印 `color`。它会立即借用（通过引用，`&`）`color` 并将该借用和
+    // 闭包本身存储到 `print` 变量中。`color` 会一直保持被借用状态直到
+    // `print` 离开作用域。
+    //
+    // `println!` 只需传引用就能使用，而这个闭包捕获的也是变量的引用，因此无需
+    // 进一步处理就可以使用 `println!`。
+    let print = || println!("`color`: {}", color);
+
+    // 使用借用来调用闭包 `color`。
+    print();
+
+    // `color` 可再次被不可变借用，因为闭包只持有一个指向 `color` 的不可变引用。
+    let _reborrow = &color;
+    print();
+
+    // 在最后使用 `print` 之后，移动或重新借用都是允许的。
+    let _color_moved = color;
+
+    let mut count = 0;
+    // 这个闭包使 `count` 值增加。要做到这点，它需要得到 `&mut count` 或者
+    // `count` 本身，但 `&mut count` 的要求没那么严格，所以我们采取这种方式。
+    // 该闭包立即借用 `count`。
+    //
+    // `inc` 前面需要加上 `mut`，因为闭包里存储着一个 `&mut` 变量。调用闭包时，
+    // 该变量的变化就意味着闭包内部发生了变化。因此闭包需要是可变的。
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    // 使用可变借用调用闭包
+    inc();
+
+    // 因为之后调用闭包，所以仍然可变借用 `count`
+    // 试图重新借用将导致错误
+    // let _reborrow = &count;
+    // ^ 试一试：将此行注释去掉。
+    inc();
+
+    // 闭包不再借用 `&mut count`，因此可以正确地重新借用
+    let _count_reborrowed = &mut count;
+
+    // 不可复制类型（non-copy type）。
+    let movable = Box::new(3);
+
+    // `mem::drop` 要求 `T` 类型本身，所以闭包将会捕获变量的值。这种情况下，
+    // 可复制类型将会复制给闭包，从而原始值不受影响。不可复制类型必须移动
+    // （move）到闭包中，因而 `movable` 变量在这里立即移动到了闭包中。
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        mem::drop(movable);
+    };
+
+    // `consume` 消耗了该变量，所以该闭包只能调用一次。
+    consume();
+    //consume();
+    // ^ 试一试：将此行注释去掉。
+
+    // `Vec` 在语义上是不可复制的。
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    //println!("There're {} elements in vec", haystack.len());
+    // ^ 取消上面一行的注释将导致编译时错误，因为借用检查不允许在变量被移动走
+    // 之后继续使用它。
+
+    // 在闭包的签名中删除 `move` 会导致闭包以不可变方式借用 `haystack`，因此之后
+    // `haystack` 仍然可用，取消上面的注释也不会导致错误。
+}
+
+fn main9_2_2() {
+    // 该函数将闭包作为参数并调用它。
+    fn apply<F>(f: F) where
+    // 闭包没有输入值和返回值。
+        F: FnOnce() {
+        // ^ 试一试：将 `FnOnce` 换成 `Fn` 或 `FnMut`。
+
+        f();
+    }
+
+    // 输入闭包，返回一个 `i32` 整型的函数。
+    fn apply_to_3<F>(f: F) -> i32 where
+    // 闭包处理一个 `i32` 整型并返回一个 `i32` 整型。
+        F: Fn(i32) -> i32 {
+
+        f(3)
+    }
+
+    use std::mem;
+
+    let greeting = "hello";
+    // 不可复制的类型。
+    // `to_owned` 从借用的数据创建有所有权的数据。
+    let mut farewell = "goodbye".to_owned();
+
+    // 捕获 2 个变量：通过引用捕获 `greeting`，通过值捕获 `farewell`。
+    let diary = || {
+        // `greeting` 通过引用捕获，故需要闭包是 `Fn`。
+        println!("I said {}.", greeting);
+
+        // 下文改变了 `farewell` ，因而要求闭包通过可变引用来捕获它。
+        // 现在需要 `FnMut`。
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // 手动调用 drop 又要求闭包通过值获取 `farewell`。
+        // 现在需要 `FnOnce`。
+        mem::drop(farewell);
+    };
+
+    // 以闭包作为参数，调用函数 `apply`。
+    apply(diary);
+
+    // 闭包 `double` 满足 `apply_to_3` 的 trait 约束。
+    let double = |x| 2 * x;
+
+    println!("3 doubled: {}", apply_to_3(double));
+}
+// todo: now pass 9.2
+
+fn main9_3() {
+    fn is_odd(n: u32) -> bool {
+        n % 2 == 1
+    }
+
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+
+    // 命令式（imperative）的写法
+    // 声明累加器变量
+    let mut acc = 0;
+    // 迭代：0，1, 2, ... 到无穷大
+    for n in 0.. {
+        // 数字的平方
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // 若大于上限则退出循环
+            break;
+        }
+
+        if is_odd(n_squared) {
+            // 如果是奇数就计数
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+
+    // 函数式的写法
+    let sum_of_squared_odd_numbers: u32 =
+        (0..).map(|n| n * n)             // 所有自然数取平方
+            .take_while(|&n| n < upper) // 取小于上限的
+            .filter(|&n| is_odd(n))     // 取奇数
+            .fold(0, |sum, i| sum + i); // 最后加起来
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
+fn main9_4() {
+    fn foo() -> ! {
+        panic!("This call never returns.");
+    }
+
+    fn some_fn() {
+        ()
+    }
+    let a: () = some_fn();
+    println!("This function returns and you can see this line.");
+
+    let x: ! = panic!("This call never returns.");
+    println!("You will never see this line!");
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            // 注意这个 match 表达式的返回值必须为 u32，
+            // 因为 “addition” 变量是这个类型。
+            let addition: u32 = match i%2 == 1 {
+                // “i” 变量的类型为 u32，这毫无问题。
+                true => i,
+                // 另一方面，“continue” 表达式不返回 u32，但它仍然没有问题，
+                // 因为它永远不会返回，因此不会违反匹配表达式的类型要求。
+                false => continue,
+            };
+            acc += addition;
+        }
+        acc
+    }
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
+}
 fn main() {
-    main8_6();
+    main9_4();
 }
