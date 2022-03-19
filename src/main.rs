dec7c669f461e61b8a4d7db560bd3331980faeb4
李赫 <lihe@lihedeMacBook-Pro.local>
2022-03-19 16:04:30 +0800

    finish champter3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,721 @@
+use std::f64::consts::PI;
+use std::{fmt, mem};
+
+use std::fmt::{DebugList, Formatter, write, Display};
+
+
+// use ferris_says::say; // from the previous step
+// use std::io::{stdout, BufWriter};
+fn main1() {
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 100);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is
+    // by providing a suffix. The number 31i64 for example has the type i64.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "lihe", "yuanjie");
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 20);
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:<width$}", number=1, width=6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:0>width$}", number=1, width=6);
+
+    // Rust even checks to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // done ^ Add the missing argument: "James"
+
+    // Create a structure named `Structure` which contains an `i32`.
+    // #[allow(dead_code)]
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // However, custom types such as this structure require more complicated
+    // handling. This will not work.
+    // println!("This struct `{}` won't print...", Structure(3));
+    // FIXME ^ Comment out this line.
+
+    let pi = PI;
+
+    println!("{number:.2}", number=pi);
+
+
+    struct Unprintable(i32);
+
+    #[derive(Debug)]
+    struct DebugPrintable(i32);
+
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    #[derive(Debug)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8
+    }
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
+
+}
+fn main1_2_2(){
+    struct Structure(i32);
+
+    impl fmt::Display for Structure {
+        // This trait requires `fmt` with this exact signature.
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Write strictly the first element into the supplied output
+            // stream: `f`. Returns `fmt::Result` which indicates whether the
+            // operation succeeded or failed. Note that `write!` uses syntax which
+            // is very similar to `println!`.
+            write!(f, "{}", self.0)
+        }
+    }
+
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+
+    // Implement `Display` for `MinMax`.
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Use `self.number` to refer to each positional data point.
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    // Define a structure where the fields are nameable for comparison.
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    // Similarly, implement `Display` for `Point2D`
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Customize so only `x` and `y` are denoted.
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range =   MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // Error. Both `Debug` and `Display` were implemented, but `{:b}`
+    // requires `fmt::Binary` to be implemented. This will not work.
+    // println!("What does Point2D look like in binary: {:b}?", point);
+    // fixme
+
+    struct Complex {
+        real: i32,
+        imag: i32
+    }
+
+    impl fmt::Display for Complex {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "read : {}, imag {}", self.real, self.imag)
+        }
+    }
+    let complex = Complex{real:5, imag: 10};
+    println!("complex {}", complex)
+
+}
+fn main1_2_2_1(){
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;
+
+            for (i, m) in vec.iter().enumerate() {
+                if i != 0 { write!(f, ",")?; };
+                write!(f, "{} : {}" , i , m)?;
+                // todo ? meaning
+            }
+
+            write!(f, "]")
+        }
+    }
+    let v = List(vec![1,2,3]);
+    print!("{}", v);
+}
+fn main1_2_3() {
+    struct City {
+        name: &'static str,
+        // Latitude
+        lat: f32,
+        // Longitude
+        lon: f32,
+    }
+
+    impl Display for City {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` is like `format!`, but it will write the formatted string
+            // into a buffer (the first argument)
+            write!(f, "{}: {:.3}°{} {:.3}°{}",
+                   self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    impl Display for Color {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "RGB ({} {} {}) ", self.red, self.green, self.blue)?;
+            write!(f, "0x{:0<2X}{:0<2X}{:0<2X}", self.red, self.green, self.blue)
+
+        }
+    }
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", *color);
+    }
+}
+
+fn main2() {
+    // 变量可以给出类型说明。
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;  // 常规说明
+    let an_integer   = 5i32; // 后缀说明
+
+    // 否则会按默认方式决定类型。
+    let default_float   = 3.0; // `f64`
+    let default_integer = 7;   // `i32`
+
+    // 类型也可根据上下文自动推断。
+    let mut inferred_type = 12; // 根据下一行的赋值推断为 i64 类型
+    inferred_type = 4294967296i64;
+
+    // 可变的（mutable）变量，其值可以改变。
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+
+    // 报错！变量的类型并不能改变。
+    // mutable = true;
+
+    // 但可以用遮蔽（shadow）来覆盖前面的变量。
+    let mutable = true;
+}
+fn main2_1() {
+    // 整数相加
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // 整数相减
+    println!("1 - 2 = {}", 1i32 - 2);
+    // 试一试 ^ 尝试将 `1i32` 改为 `1u32`，体会为什么类型声明这么重要
+
+    // 短路求值的布尔逻辑
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // 位运算
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // 使用下划线改善数字的可读性！
+    println!("One million is written as {}", 1_000_000u32);
+}
+fn main2_2() {
+    // 元组可以充当函数的参数和返回值
+    fn reverse(pair: (i32, bool)) -> (bool, i32) {
+        // 可以使用 `let` 把一个元组的成员绑定到一些变量
+        let (integer, boolean) = pair;
+
+        (boolean, integer)
+    }
+
+    // 在 “动手试一试” 的练习中要用到下面这个结构体。
+    #[derive(Debug)]
+    struct Matrix(f32, f32, f32, f32);
+
+    // 包含各种不同类型的元组
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // 通过元组的下标来访问具体的值
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    // 元组也可以充当元组的元素
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // 元组可以打印
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // 但很长的元组无法打印
+    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+    // 试一试 ^ 取消上面两行的注释，阅读编译器给出的错误信息。
+    // todo why?
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // 创建单元素元组需要一个额外的逗号，这是为了和被括号包含的字面量作区分。
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    // 元组可以被解构（deconstruct），从而将值绑定给变量
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+
+    impl Display for Matrix {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+        }
+    }
+    println!("{}", matrix);
+    println!("{:?}", matrix);
+
+    fn transpose(mut matrix: Matrix) -> Matrix {
+        let tmp = matrix.0;
+        matrix.0 = matrix.3;
+        matrix.3 = tmp;
+        // let res = Matrix(matrix.3, matrix.1, matrix.3, matrix.0);
+        // println!("res : {}", res);
+        matrix
+    }
+
+    println!("{}", transpose(matrix));
+}
+fn main2_3() {
+    // 此函数借用一个 slice
+    fn analyze_slice(slice: &[i32]) {
+        println!("first element of the slice: {}", slice[0]);
+        println!("the slice has {} elements", slice.len());
+    }
+
+    // 定长数组（类型标记是多余的）
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let xs1 = [5, 4, 3, 2, 1];
+
+    // 所有元素可以初始化成相同的值
+    let ys: [i32; 500] = [0; 500];
+    let ys1 = [0, 500];
+
+    // 下标从 0 开始
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+
+    // `len` 返回数组的大小
+    println!("array size: {}", xs.len());
+
+    // 数组是在栈中分配的
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // 数组可以自动被借用成为 slice
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // slice 可以指向数组的一部分
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1 .. 4]);
+
+    // 越界的下标会引发致命错误（panic）
+    // println!("{}", xs[5]);
+}
+
+fn main3_1() {
+
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: u8
+    }
+    // 单元结构体
+    struct Unit;
+
+    // 元组结构体
+    struct Pair(i32, f32);
+
+    // 带有两个字段的结构体
+    #[derive(Debug)]
+    struct Point {
+        x: f32,
+        y: f32,
+    }
+
+    // 结构体可以作为另一个结构体的字段
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct Rectangle {
+        // 可以在空间中给定左上角和右下角在空间中的位置来指定矩形。
+        top_left: Point,
+        bottom_right: Point,
+    }
+
+    // 使用简单的写法初始化字段，并创建结构体
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    // 以 Debug 方式打印结构体
+    println!("{:?}", peter);
+
+    // 实例化结构体 `Point`
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    // 访问 point 的字段
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // 使用结构体更新语法创建新的 point，
+    // 这样可以用到之前的 point 的字段
+    let bottom_right = Point { x: 5.2, y: 5.3 };
+    // ..point? 缺省的就一样
+
+    // `new_point.y` 与 `point.y` 一样，因为这个字段就是从 `point` 中来的
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // 使用 `let` 绑定来解构 point
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        // 结构体的实例化也是一个表达式
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    // 实例化一个单元结构体
+    let _unit = Unit;
+
+    // 实例化一个元组结构体
+    let pair = Pair(1, 0.1);
+
+    // 访问元组结构体的字段
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // 解构一个元组结构体
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+    fn abs(mut num: f32) -> f32 {
+        if num < 0.0f32 {
+            num = -num;
+        }
+        num
+
+    }
+
+    fn rect_area(rect: Rectangle) -> f32 {
+        let Rectangle{ top_left, bottom_right }: Rectangle = rect;
+        abs(top_left.x - bottom_right.x) * abs(top_left.y - bottom_right.y)
+    }
+
+    fn square(point: Point, len: f32) -> Rectangle {
+        let x = point.x;
+        let y = point.y;
+        Rectangle {
+            top_left: point,
+            bottom_right: Point {
+                x: x + len,
+                y: y + len
+            }
+        }
+    }
+
+    unsafe { println!("test rect_area: {}", rect_area(_rectangle)); }
+    println!("test square :{:?}", square(point, 10.0));
+
+}
+
+
+fn main3_2() {
+    #![allow(dead_code)]
+    // 创建一个 `enum`（枚举）来对 web 事件分类。注意变量名和类型共同指定了 `enum`
+    // 取值的种类：`PageLoad` 不等于 `PageUnload`，`KeyPress(char)` 不等于
+    // `Paste(String)`。各个取值不同，互相独立。
+    enum WebEvent {
+        // 一个 `enum` 可以是单元结构体（称为 `unit-like` 或 `unit`），
+        PageLoad,
+        PageUnload,
+        // 或者一个元组结构体，
+        KeyPress(char),
+        Paste(String),
+        // 或者一个普通的结构体。
+        Click { x: i64, y: i64 }
+    }
+
+    // 此函数将一个 `WebEvent` enum 作为参数，无返回值。
+    fn inspect(event: WebEvent) {
+        match event {
+            WebEvent::PageLoad => println!("page loaded"),
+            WebEvent::PageUnload => println!("page unloaded"),
+            // 从 `enum` 里解构出 `c`。
+            WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+            WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+            // 把 `Click` 解构给 `x` and `y`。
+            WebEvent::Click { x, y } => {
+                println!("clicked at x={}, y={}.", x, y);
+            },
+        }
+    }
+
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` 从一个字符串切片中创建一个具有所有权的 `String`。
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y: 80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+
+    enum VeryVerboseEnumOfThingsToDoWithNumbers {
+        Add,
+        Subtract,
+    }
+
+    // Creates a type alias
+    type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+    // We can refer to each variant via its alias, not its long and inconvenient
+    // name.
+    let x = Operations::Add;
+
+    impl VeryVerboseEnumOfThingsToDoWithNumbers {
+        fn run(&self, x: i32, y: i32) -> i32 {
+            match self {
+                Self::Add => x + y,
+                Self::Subtract => x - y,
+            }
+        }
+    }
+
+    println!("{}", x.run(1, 5));
+
+}
+
+fn main3_2_1() {
+    enum Status {
+        Rich,
+        Poor,
+    }
+
+    enum Work {
+        Civilian,
+        Soldier,
+    }
+    // 显式地 `use` 各个名称使他们直接可用，而不需要指定它们来自 `Status`。
+    use Status::{Poor, Rich};
+    // 自动地 `use` `Work` 内部的各个名称。
+    use Work::*;
+
+    // `Poor` 等价于 `Status::Poor`。
+    let status = Poor;
+    // `Civilian` 等价于 `Work::Civilian`。
+    let work = Civilian;
+
+    match status {
+        // 注意这里没有用完整路径，因为上面显式地使用了 `use`。
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        // 再次注意到没有用完整路径。
+        Civilian => println!("Civilians work!"),
+        Soldier  => println!("Soldiers fight!"),
+    }
+}
+use List::*;
+fn main3_2_2() {
+    // 拥有隐式辨别值（implicit discriminator，从 0 开始）的 enum
+    enum Number {
+        Zero,
+        One,
+        Two,
+    }
+
+    // 拥有显式辨别值（explicit discriminator）的 enum
+    enum Color {
+        Red = 0xff0000,
+        Green = 0x00ff00,
+        Blue = 0x0000ff,
+    }
+    // `enum` 可以转成整型。
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+}
+enum List {
+    // Cons：元组结构体，包含链表的一个元素和一个指向下一节点的指针
+    Cons(u32, Box<List>),
+    // Nil：末结点，表明链表结束
+    Nil,
+}
+
+// 可以为 enum 定义方法
+impl List {
+    // 创建一个空的 List 实例
+    fn new() -> List {
+        // `Nil` 为 `List` 类型（译注：因 `Nil` 的完整名称是 `List::Nil`）
+        Nil
+    }
+
+    // 处理一个 List，在其头部插入新元素，并返回该 List
+    fn prepend(self, elem: u32) -> List {
+        // `Cons` 同样为 List 类型
+        Cons(elem, Box::new(self))
+    }
+
+    // 返回 List 的长度
+    fn len(&self) -> u32 {
+        // 必须对 `self` 进行匹配（match），因为这个方法的行为取决于 `self` 的
+        // 取值种类。
+        // `self` 为 `&List` 类型，`*self` 为 `List` 类型，匹配一个具体的 `T`
+        // 类型要好过匹配引用 `&T`。
+        match *self {
+            // 不能得到 tail 的所有权，因为 `self` 是借用的；
+            // 因此使用一个对 tail 的引用
+            Cons(_, ref tail) => 1 + tail.len(),
+            // （递归的）基准情形（base case）：一个长度为 0 的空列表
+            Nil => 0
+        }
+    }
+
+    // 返回列表的字符串表示（该字符串是堆分配的）
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                // `format!` 和 `print!` 类似，但返回的是一个堆分配的字符串，
+                // 而不是打印结果到控制台上
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
+fn main3_2_3() {
+
+
+    // 创建一个空链表
+    let mut list = List::new();
+
+    // 追加一些元素
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    // 显示链表的最后状态
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}
+
+fn main3_3() {
+    // 全局变量是在在所有其他作用域之外声明的。
+    static LANGUAGE: &'static str = "Rust";
+    static LANGUAGE1: &str = "Rust1";
+    const  THRESHOLD: i32 = 10;
+
+    fn is_big(n: i32) -> bool {
+        // 在一般函数中访问常量
+        n > THRESHOLD
+    }
+
+    let n = 16;
+
+    // 在 main 函数（主函数）中访问常量
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // 报错！不能修改一个 `const` 常量。
+    // THRESHOLD = 5;
+    // 改正 ^ 注释掉此行
+    LANGUAGE = "C++";
+}
+
 fn main() {
-    println!("Hello, world!");
+    main3_3();
 }
